9fba9bc4b4111f07e2b0fe22334635d57510ee19
Duncan Coutts <duncan@well-typed.com>
2020-05-27 16:02:40 +0100

    Initial typed API
    Initially focusing on creating and serialisaing keys

---
+++ cardano-api/test/Test/Cardano/Api/Typed.hs
@@ -0,0 +1,48 @@
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+module Test.Cardano.Api.Typed
+  ( tests
+  ) where
+
+import           Cardano.Api.Typed
+
+import           Cardano.Prelude
+
+import           Hedgehog (Property, discover)
+import qualified Hedgehog as H
+
+import           Test.Cardano.Api.Gen (genSeed)
+import           Test.Cardano.Api.Orphans ()
+
+
+prop_roundtrip_VrfVerificationKey_envelope :: Property
+prop_roundtrip_VrfVerificationKey_envelope =
+  H.property $ do
+    vkey <- H.forAll (genVerificationKey AsVrfKey)
+    H.tripping vkey (serialiseToTextEnvelope Nothing)
+                    (deserialiseFromTextEnvelope (AsVerificationKey AsVrfKey))
+
+-- -----------------------------------------------------------------------------
+
+genSigningKey :: Key keyrole => AsType keyrole -> H.Gen (SigningKey keyrole)
+genSigningKey ttoken = do
+  seed <- genSeed (fromIntegral seedSize)
+  let sk = deterministicSigningKey ttoken seed
+  return sk
+  where
+    seedSize :: Word
+    seedSize = deterministicSigningKeySeedSize ttoken
+
+genVerificationKey :: Key keyrole
+                   => AsType keyrole -> H.Gen (VerificationKey keyrole)
+genVerificationKey ttoken = getVerificationKey <$> genSigningKey ttoken
+
+
+deriving instance Eq (VerificationKey VrfKey)
+
+
+tests :: IO Bool
+tests =
+  H.checkParallel $$discover
